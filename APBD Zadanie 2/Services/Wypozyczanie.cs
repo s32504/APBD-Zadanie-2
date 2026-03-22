650ffe44931d87c74ba1a126401b12a64ce94db0
Michał Brzóska <s32504@pjwstk.edu.pl>
2026-03-22 18:13:20 +0100

    >Dodanie klasy Wypozyczanie.cs obsługującej wypożyczanie i zwroty

---
+++ APBD Zadanie 2/Services/Wypozyczanie.cs
@@ -0,0 +1,27 @@
+﻿namespace APBD_Zadanie_2.Services;
+using APBD_Zadanie_2.Domena;
+using APBD_Zadanie_2.Enums;
+
+class Wypozyczanie
+{
+    public Aktor Aktor { get; }
+    public Sprzet Sprzet { get; }
+    public DateTime DataWypozyczenia { get; }
+    public DateTime DataOczekiwanegoZwrotu { get; }
+    public DateTime? DataZwrotu { get; private set; }
+
+    public Wypozyczanie(Aktor aktor, Sprzet sprzet, int days)
+    {
+        Aktor = aktor;
+        Sprzet = sprzet;
+        DataWypozyczenia = DateTime.Now;
+        DataOczekiwanegoZwrotu = DataWypozyczenia.AddDays(days);
+
+        Sprzet.Status = Stan.Wypożyczony;
+    }
+    public void Return()
+    {
+        DataZwrotu = DateTime.Now;
+        Sprzet.Status = Stan.Dostępny;
+    }
+}
